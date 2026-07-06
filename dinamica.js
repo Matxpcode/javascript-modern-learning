45a535bf9c28f054f6bebc61461cc9862cadb554
MateoHuarcaya <codercmateocoder@gmail.com>
2026-07-06 07:53:36 -0500

    feat: agregar regla de descuento en js, ademas agregar 2 filas mas al card

--- dinamica.js
+++ dinamica.js
@@ -17,7 +17,7 @@ const nombre = document.getElementById("nombre");
 const precio = document.getElementById("precio");
 
 nombre.addEventListener('input', () => {
-    if (nombre.value !== "") {
+    if (nombre.value.trim() !== "") {
         document.getElementById("alerta_nombre").classList.add('d-none');
     } else {
         document.getElementById("alerta_nombre").classList.remove('d-none');
@@ -25,28 +25,71 @@ nombre.addEventListener('input', () => {
 });
 
 precio.addEventListener('input', () => {
-    const valor = precio.value;
+    const valor = precio.value.trim();
 
-    if (valor===""||parseFloat(valor)<=0) {
+    if (valor === "" || parseFloat(valor) <= 0) {
         document.getElementById("alerta_precio").classList.remove('d-none');
     } else {
         document.getElementById("alerta_precio").classList.add('d-none');
     }
 });
 
-document.querySelector("form").addEventListener("submit",function(e){
+document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault();
     if (!opcionEscogida) {
         document.getElementById("alerta_tipo").classList.remove('d-none');
         return;
-    }else{
-        // document.getElementById("alerta_tipo").classList.add('d-none');
+    } else {
         document.getElementById("formulario").classList.add("d-none")
-        document.getElementById("tarjeta").classList.remove("d-none"); 
+        document.getElementById("tarjeta").classList.remove("d-none");
         document.getElementById("alerta_completada").classList.remove("d-none");
 
         document.getElementById("result_nombre").textContent = nombre.value;
         document.getElementById("result_precio").textContent = precio.value;
         document.getElementById("result_tipo").textContent = opcionEscogida;
+        
+        document.getElementById("descuento").textContent = calcularTodo().porcentaje;
+        document.getElementById("total").textContent = calcularTodo().total;
     }
 });
+
+// Reglas de descuento
+
+//1.Primera opcion, pero menos eficiente por scope
+// let descuento="";
+
+// function total(){
+//     if (opcionEscogida === "nuevo") {
+//         descuento="5%";        
+//         return parseFloat(precio.value) * 0.95;
+//     } else if (opcionEscogida === "regular") {
+//         descuento="10%";
+//         return parseFloat(precio.value) * 0.9;
+//     } else {
+//         descuento="20%"
+//         return parseFloat(precio.value) * 0.8;
+//     }
+// }
+
+//2.Segunda opcion, mas factible, todo en un objeto
+function calcularTodo() {
+    let precioNumerico = parseFloat(precio.value.trim());
+    let desc = "";
+    let neto = 0;
+
+    if (opcionEscogida==="nuevo") {
+        desc="5%";
+        neto=precioNumerico*0.95;
+    }else if(opcionEscogida==="regular"){
+        desc="10%";
+        neto=precioNumerico*0.9;
+    }else{
+        desc="20%";
+        neto=precioNumerico*0.8;
+    }
+
+    return{
+        porcentaje:desc,
+        total:neto
+    }
+}
